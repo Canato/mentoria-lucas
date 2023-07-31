c5c2e2ff828b54280e64ae109f7e08ea26498e1c
krawcts <116924698+krawcts@users.noreply.github.com>
2023-07-31 13:32:46 -0300

    wip
    (cherry picked from commit d453f1b3e32c205d00c84b131e4feaeea1c7ae58)

--- app/src/main/java/com/monzo/androidtest/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/monzo/androidtest/articles/ArticlesViewModel.kt
@@ -7,29 +7,43 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class ArticlesViewModel(
-        private val repository: ArticlesRepository
+    private val repository: ArticlesRepository
 ) : BaseViewModel<ArticlesState>(ArticlesState()) {
     init {
         disposables += repository.latestFintechArticles()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { articles ->
-                    setState { copy(refreshing = false, articles = articles) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { articles ->
+                val newArticle = articles.mapIndexed { index, article ->
+                    if (index % 3 == 0) {
+                        article.copy(title = "LUCAS")
+                    } else article
+
                 }
+                state.value?.copy(refreshing = false, articles = newArticle)?.let { newState ->
+                    setState(newState)
+                }
+            }
     }
 
     fun onRefresh() {
-        setState { copy(refreshing = true) }
+        state.value?.copy(refreshing = true)?.let { newState -> setState(newState) }
         disposables += repository.latestFintechArticles()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { articles ->
-                    setState { copy(articles = articles) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { articles ->
+                val newArticle = articles.mapIndexed { index, article ->
+                    if (index % 3 == 0) {
+                        article.copy(title = "LUCAS")
+                    } else article
                 }
+                state.value?.copy(articles = newArticle)?.let { newState -> setState(newState) }
+            }
     }
 }
 
+
 data class ArticlesState(
-        val refreshing: Boolean = true,
-        val articles: List<Article> = emptyList()
+    val refreshing: Boolean = true,
+    val articles: List<Article> = emptyList()
 )
