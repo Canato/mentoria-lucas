779e20137093d8b8dc67e999c68dbb826f9649da
krawcts <116924698+krawcts@users.noreply.github.com>
2023-08-07 10:30:42 -0300

    Create private function to change articles Title

--- app/src/main/java/com/monzo/androidtest/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/monzo/androidtest/articles/ArticlesViewModel.kt
@@ -9,35 +9,29 @@ import io.reactivex.schedulers.Schedulers
 class ArticlesViewModel(
     private val repository: ArticlesRepository
 ) : BaseViewModel<ArticlesState>(ArticlesState()) {
+    private fun changeArticlesTitle (articles: List<Article>, step: Int = 0, refresh: Boolean) {
+        val newArticle = articles.mapIndexed { index, article ->
+            if (index % step == 0) {
+                article.copy(title = "LUCAS")
+            } else article
+
+        }
+        state.value?.copy(refreshing = refresh, articles = newArticle)?.let { newState ->
+            setState(newState)
+        }
+    }
     init {
         disposables += repository.latestFintechArticles()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { articles ->
-                val newArticle = articles.mapIndexed { index, article ->
-                    if (index % 3 == 0) {
-                        article.copy(title = "LUCAS")
-                    } else article
+            .subscribe { articles -> changeArticlesTitle(articles, step = 2, refresh = false) }
 
-                }
-                state.value?.copy(refreshing = false, articles = newArticle)?.let { newState ->
-                    setState(newState)
-                }
-            }
     }
-
     fun onRefresh() {
-        state.value?.copy(refreshing = true)?.let { newState -> setState(newState) }
         disposables += repository.latestFintechArticles()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { articles ->
-                val newArticle = articles.mapIndexed { index, article ->
-                    if (index % 3 == 0) {
-                        article.copy(title = "LUCAS")
-                    } else article
-                }
-                state.value?.copy(articles = newArticle)?.let { newState -> setState(newState) }
+            .subscribe { articles -> changeArticlesTitle(articles, step = 5, refresh = true)
             }
     }
 }
