e6291aeed6924caa9d6b59fe84e72880d73fc1b1
krawcts <116924698+krawcts@users.noreply.github.com>
2023-08-23 10:47:40 -0300

    create new activity

---
+++ app/src/main/java/com/monzo/androidtest/individual/presentation/IndividualArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.monzo.androidtest.individual.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.monzo.androidtest.R
+
+class IndividualArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual_article)
+    }
+}
