d0bee04889b140a80da441addbe93bf37d4e5d1a
krawcts <116924698+krawcts@users.noreply.github.com>
2023-08-26 17:51:05 -0300

    formating the code

--- app/src/main/java/com/monzo/androidtest/articles/presentation/ArticleAdapter.kt
+++ app/src/main/java/com/monzo/androidtest/articles/presentation/ArticleAdapter.kt
@@ -14,19 +14,20 @@ import java.util.*
 
 
 internal class ArticleAdapter(
-        private val context: Context
+    private val context: Context
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val articles: MutableList<ArticleItem> = ArrayList()
 
-    private lateinit var mlistener: onItemClickListener
-    interface onItemClickListener{
+    private lateinit var clickListener: OnItemClickListener
+
+    interface OnItemClickListener {
         fun onItemClick(position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflater = LayoutInflater.from(context)
         val view = layoutInflater.inflate(R.layout.list_item_article, parent, false)
-        return ArticleViewHolder(view, context, mlistener)
+        return ArticleViewHolder(view, context, clickListener)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -44,11 +45,15 @@ internal class ArticleAdapter(
         notifyDataSetChanged()
     }
 
-    fun setOnItemClickListener(listener: onItemClickListener){
-        mlistener = listener
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        clickListener = listener
     }
 
-    class ArticleViewHolder(view: View, private val context: Context, listener: onItemClickListener) : RecyclerView.ViewHolder(view) {
+    class ArticleViewHolder(
+        view: View,
+        private val context: Context,
+        listener: OnItemClickListener
+    ) : RecyclerView.ViewHolder(view) {
         fun bind(article: ArticleItem) {
             val headlineView = itemView.findViewById<TextView>(R.id.article_headline_textview)
             val thumbnailView = itemView.findViewById<ImageView>(R.id.article_thumbnail_imageview)
@@ -58,7 +63,8 @@ internal class ArticleAdapter(
             dateArticleView.text = article.published
             Glide.with(context).load(article.thumbnail).into(thumbnailView)
         }
-        init{
+
+        init {
             itemView.setOnClickListener {
                 listener.onItemClick(bindingAdapterPosition)
             }
