a2247c80a868cb03e422027cbceeb6eb40185e5c
krawcts <116924698+krawcts@users.noreply.github.com>
2023-08-31 18:06:39 -0300

    configure api call for new activity

--- app/src/main/java/com/monzo/androidtest/HeadlinesApp.kt
+++ app/src/main/java/com/monzo/androidtest/HeadlinesApp.kt
@@ -3,13 +3,18 @@ package com.monzo.androidtest
 import android.app.Application
 import android.content.Context
 import com.monzo.androidtest.articles.ArticlesModule
+import com.monzo.androidtest.individual.IndividualModule
 
 class HeadlinesApp : Application() {
     private val articlesModule = ArticlesModule()
+    private val individualModule = IndividualModule()
 
     companion object {
         fun from(applicationContext: Context): ArticlesModule {
             return (applicationContext as HeadlinesApp).articlesModule
+        }
+        fun from2(applicationContext: Context): IndividualModule{
+            return (applicationContext as HeadlinesApp).individualModule
         }
     }
 }
