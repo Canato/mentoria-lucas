0516249127098634d66db7d8edad742c7028f793
krawcts <116924698+krawcts@users.noreply.github.com>
2023-08-31 18:09:50 -0300

    configure activity ViewModel

--- app/src/main/java/com/monzo/androidtest/individual/presentation/IndividualState.kt
+++ app/src/main/java/com/monzo/androidtest/individual/presentation/IndividualState.kt
@@ -1,2 +1,13 @@
 package com.monzo.androidtest.individual.presentation
 
+
+
+data class IndividualState(
+    val article: IndividualArticle? = null
+)
+
+data class IndividualArticle(
+    val thumbnail : String,
+    val headline : String,
+    val text : String
+)
--- app/src/main/java/com/monzo/androidtest/individual/presentation/IndividualViewModel.kt
+++ app/src/main/java/com/monzo/androidtest/individual/presentation/IndividualViewModel.kt
@@ -1,4 +1,34 @@
 package com.monzo.androidtest.individual.presentation
 
-class IndividualViewModel {
+import android.widget.Toast
+import com.monzo.androidtest.common.BaseViewModel
+import com.monzo.androidtest.individual.data.IndvArticlesRepository
+import com.monzo.androidtest.common.plusAssign
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+
+
+class IndividualViewModel(
+    private val repository: IndvArticlesRepository,
+    private val url: String
+) : BaseViewModel<IndividualState>(IndividualState()) {
+
+
+    init {
+        disposables += repository.getArticle(url).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe ({ article ->
+                val newArticle = IndividualArticle(
+                    thumbnail = article.thumbnail,
+                    headline = article.headline,
+                    text = article.body
+                )
+                state.value?.copy(article = newArticle)?.let { newState ->
+                    setState(newState)
+                }
+
+            }, Timber::e)
+
+    }
 }
