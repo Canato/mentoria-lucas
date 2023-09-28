aef08d9ef11d61b8a7a4023ac4807f8065370057
krawcts <116924698+krawcts@users.noreply.github.com>
2023-09-28 15:51:48 -0300

    ViewModel now receives the articles divided in sections

--- app/src/main/java/com/monzo/androidtest/articles/presentation/ArticleSectionMapper.kt
+++ app/src/main/java/com/monzo/androidtest/articles/presentation/ArticleSectionMapper.kt
@@ -1,4 +1,60 @@
 package com.monzo.androidtest.articles.presentation
 
-class ArticleSectionMapper {
+import com.monzo.androidtest.articles.domain.Article
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+
+object ArticleSectionMapper {
+    fun mapToSection(articles: List<Article>): List<ArticleSection> {
+
+        val currentDate = Date()
+        val calendar = Calendar.getInstance()
+
+        calendar.time = currentDate
+
+        val currentWeek = calendar.get(Calendar.WEEK_OF_YEAR)
+
+        val articlesThisWeek = mutableListOf<Article>()
+        val articlesLastWeek = mutableListOf<Article>()
+        val articlesOlder = mutableListOf<Article>()
+
+        for (article in articles) {
+            calendar.time = article.published
+            val articleWeek = calendar.get(Calendar.WEEK_OF_YEAR)
+            val weekDiff = currentWeek - articleWeek
+
+            if (weekDiff == 0) {
+                articlesThisWeek.add(article)
+            } else if (weekDiff == 1) {
+                articlesLastWeek.add(article)
+            } else {
+                articlesOlder.add(article)
+            }
+        }
+        val articleItemThisWeek = mapToArticleItem(articlesThisWeek)
+        val articleItemLastWeek = mapToArticleItem(articlesLastWeek)
+        val articleItemOlder = mapToArticleItem(articlesOlder)
+
+
+
+        return listOf(
+            ArticleSection("This Week", articleItemThisWeek),
+            ArticleSection("Last Week", articleItemLastWeek),
+            ArticleSection("Older Articles", articleItemOlder)
+        )
+
+
+    }
+
+    private fun mapToArticleItem(mutableList: MutableList<Article>): List<ArticleItem> =
+        mutableList.map { article ->
+            ArticleItem(
+                thumbnail = article.thumbnail,
+                published = SimpleDateFormat("dd/MM/yyyy").run { format(article.published) },
+                title = article.title,
+                url = article.url
+            )
+        }
+
 }
--- app/src/main/java/com/monzo/androidtest/articles/presentation/ArticlesViewModel.kt
+++ app/src/main/java/com/monzo/androidtest/articles/presentation/ArticlesViewModel.kt
@@ -11,30 +11,14 @@ import java.text.SimpleDateFormat
 class ArticlesViewModel(
     private val repository: ArticlesRepository
 ) : BaseViewModel<ArticlesState>(ArticlesState()) {
-    private fun changeArticlesTitle(articles: List<ArticleItem>, step: Int = 0): List<ArticleItem> {
-        val newArticle = articles.mapIndexed { index, article ->
-            if (index % step == 0) {
-                article.copy(title = "LUCAS")
-            } else article
-        }
-        return newArticle
-    }
-
-
     init {
         disposables += repository.latestFintechArticles().subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { articles ->
-                val articlesItemList = articles.map { article ->
-                    ArticleItem(
-                        thumbnail = article.thumbnail,
-                        published = SimpleDateFormat("dd/MM/yyyy").run { format(article.published) },
-                        title = article.title,
-                        url = article.url
-                    )
-                }
-                val newArticles = changeArticlesTitle(articlesItemList, step = 2)
-                state.value?.copy(refreshing = false, articles = newArticles)?.let { newState ->
+
+                val sections = ArticleSectionMapper.mapToSection(articles)
+
+                state.value?.copy(refreshing = false, articleSection = sections)?.let { newState ->
                     setState(newState)
                 }
             }
@@ -47,21 +31,13 @@ class ArticlesViewModel(
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { articles ->
-                val articlesItemList = articles.map { article ->
-                    ArticleItem(
-                        thumbnail = article.thumbnail,
-                        published = SimpleDateFormat("dd/MM/yyyy").run { format(article.published) },
-                        title = article.title,
-                        url = article.url
-                    )
-                }
-                val newArticles = changeArticlesTitle(articlesItemList, step = 5)
-                state.value?.copy(refreshing = false, articles = newArticles)?.let { newState ->
+
+                val sections = ArticleSectionMapper.mapToSection(articles)
+
+                state.value?.copy(refreshing = false, articleSection = sections)?.let { newState ->
                     setState(newState)
                 }
             }
     }
+
 }
-
-
-
