8121b1042fc3ae8491b96093314a43a2456c57ad
krawcts <116924698+krawcts@users.noreply.github.com>
2023-10-17 01:59:55 -0300

    better solution, don't using viewHolder as an inner class.

--- app/src/main/java/com/monzo/androidtest/articles/presentation/ArticleAdapter.kt
+++ app/src/main/java/com/monzo/androidtest/articles/presentation/ArticleAdapter.kt
@@ -9,10 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.monzo.androidtest.R
-
-import java.util.*
 import kotlin.collections.ArrayList
-
 
 internal class ArticleAdapter(
     private val context: Context,
@@ -28,7 +25,8 @@ internal class ArticleAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflater = LayoutInflater.from(context)
         val view = layoutInflater.inflate(R.layout.list_item_article, parent, false)
-        return ArticleViewHolder(view, context, listener)
+
+        return ArticleViewHolder(view, context) { listener.onItemClick(articles[it].url) }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -46,10 +44,10 @@ internal class ArticleAdapter(
         notifyDataSetChanged()
     }
 
-    inner class ArticleViewHolder(
+    class ArticleViewHolder(
         view: View,
         private val context: Context,
-        listener: OnItemClickListener
+        getArticlePosition: (Int) -> Unit
     ) : RecyclerView.ViewHolder(view) {
         fun bind(article: ArticleItem) {
             val headlineView = itemView.findViewById<TextView>(R.id.article_headline_textview)
@@ -63,7 +61,7 @@ internal class ArticleAdapter(
 
         init {
             itemView.setOnClickListener {
-                listener.onItemClick(articles[bindingAdapterPosition].url)
+                getArticlePosition(bindingAdapterPosition)
             }
         }
     }
