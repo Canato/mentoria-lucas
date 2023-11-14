44d4575112e22583dac393bc52f6c53ebadf8409
krawcts <116924698+krawcts@users.noreply.github.com>
2023-11-14 20:59:49 -0300

    define test cases for toCamelCase function

--- app/src/test/java/com/monzo/androidtest/articles/presentation/ToCamelCaseTest.kt
+++ app/src/test/java/com/monzo/androidtest/articles/presentation/ToCamelCaseTest.kt
@@ -1,4 +1,68 @@
 package com.monzo.androidtest.articles.presentation
 
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+
 class ToCamelCaseTest {
+
+    @Test
+    fun `GIVEN value camelCase WHEN camelCase THEN correct value`() {
+        val message = "fortuneFavorsTheBold"
+        val expected = "fortuneFavorsTheBold"
+
+        val result = message.toCamelCase()
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `GIVEN value all upper case WHEN camelCase THEN correct value`() {
+        val message = "fortune FAVORS TheBOLD"
+        val expected = "fortuneFAVORSTheBOLD"
+
+        val result = message.toCamelCase()
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `GIVEN value with spaces WHEN camelCase THEN correct value`() {
+        val message = "Fortune favors The bold"
+        val expected = "fortuneFavorsTheBold"
+
+        val result = message.toCamelCase()
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `GIVEN value with underline WHEN camelCase THEN correct value`() {
+        val message = "Fortune_favors_The_bold"
+        val expected = "fortuneFavorsTheBold"
+
+        val result = message.toCamelCase()
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `GIVEN value with dash WHEN camelCase THEN correct value`() {
+        val message = "Fortune-favors-The-bold"
+        val expected = "fortuneFavorsTheBold"
+
+        val result = message.toCamelCase()
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `GIVEN value with mix WHEN camelCase THEN correct value`() {
+        val message = "Fortune_favors The-boldExtra"
+        val expected = "fortuneFavorsTheBoldExtra"
+
+        val result = message.toCamelCase()
+
+        assertEquals(expected, result)
+    }
 }
