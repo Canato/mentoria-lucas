2d9f0a22d07a3b1b37bf17d383b00eaf5dc6aa3f
krawcts <116924698+krawcts@users.noreply.github.com>
2023-11-15 10:47:39 -0300

    Implement toCamelCase function

--- app/src/main/java/com/monzo/androidtest/articles/presentation/ToCamelCase.kt
+++ app/src/main/java/com/monzo/androidtest/articles/presentation/ToCamelCase.kt
@@ -1,2 +1,20 @@
 package com.monzo.androidtest.articles.presentation
 
+fun String.toCamelCase(): String {
+    val words = this.replace("[ _-]".toRegex(), " ").splitToSequence(" ")
+
+
+    return words.joinToString("") {
+        if (it == words.first()) it.replaceFirstChar(Char::lowercaseChar)
+        else it.replaceFirstChar (Char::uppercaseChar)
+    }
+
+//    return buildString {
+//        words.forEachIndexed { index, word ->
+//            append(
+//                if (index == 0) word.replaceFirstChar{it.lowercase()}
+//                else word.replaceFirstChar{it.uppercase()}
+//            )
+//        }
+//    }
+}
